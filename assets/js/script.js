fed99780a5edbb82868c9b2dfd1ef1ef0f3138b5
SimoneCerri <iosonosimone@hotmail.it>
2024-02-20 17:17:18 +0100

    fix the log on console of values after click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,19 @@
 //Create some variables and take it from html
-let fullName = document.querySelector(".full_name").value;
-//let fullNameValue = fullName.value;
-
-let kiloMeters = document.querySelector(".kilometers").value;
-//let kiloMetersValue = kiloMeters.value;
-
+let fullName = document.querySelector(".full_name");
+let kiloMeters = document.querySelector(".kilometers");
 let submit = document.querySelector(".submit_form");
 let reset = document.querySelector(".reset_form");
 
 //Add the click event to get variables
 //document.addEventListener("click",)
 submit.addEventListener("click" , function() {
-    console.log(fullName);
-    console.log(kiloMeters);
+    let fullNameValue = fullName.value;
+    let kiloMetersValue = kiloMeters.value;
+
+    console.log(fullNameValue);
+    console.log(kiloMetersValue);
+
     let result = document.querySelector(".ticket");
-    result.append(fullName);
+
+    result.append(" " + fullNameValue);
 });

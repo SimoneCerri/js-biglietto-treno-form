01438b52ead36170d580b46e5b016eb398b443cc
SimoneCerri <iosonosimone@hotmail.it>
2024-02-20 15:28:27 +0100

    try the .value but not working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 //Create some variables and take it from html
 let fullName = document.getElementsByName("fullname");
-console.log(fullName);
+let fullNameValue = fullName.value;
+console.log(fullNameValue);
 
 //Add the click event to get variables
 //document.addEventListener("click",)

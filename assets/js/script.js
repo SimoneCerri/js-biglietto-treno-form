e69c4b609c83ec4ab2011429ec30779027b11f82
SimoneCerri <iosonosimone@hotmail.it>
2024-02-20 17:54:55 +0100

    add discount and final price with some console.log for debugging

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,17 +3,44 @@ let fullName = document.querySelector(".full_name");
 let kiloMeters = document.querySelector(".kilometers");
 let submit = document.querySelector(".submit_form");
 let reset = document.querySelector(".reset_form");
+let ageDiscount = document.getElementById("ages");
+let discount, price;
 
 //Add the click event to get variables
-//document.addEventListener("click",)
-submit.addEventListener("click" , function() {
+submit.addEventListener("click", function () {
     let fullNameValue = fullName.value;
     let kiloMetersValue = kiloMeters.value;
-
+    let ageDiscountValue = ageDiscount.value;
     console.log(fullNameValue);
     console.log(kiloMetersValue);
+    console.log(ageDiscountValue);
 
     let result = document.querySelector(".ticket");
+    price = (kiloMeters.value) * 0.21;
+    console.log(price);
+    let final_price,discount;
 
-    result.append(" " + fullNameValue);
+    if (ageDiscountValue < 18) //Under 18 discount and final price
+    {
+        discount = price * 0.2;
+        console.log(discount);
+        final_price = price - discount;
+        console.log(final_price);
+    }
+    else if (ageDiscountValue >= 65) //Over 65 discount and final price
+    {
+        discount = price * 0.4;
+        console.log(discount);
+        final_price = price - discount;
+        console.log(final_price);
+    } else //Last case between 18 and 66 yo
+    {
+        discount = 0;
+        console.log(discount);
+        final_price = price;
+        console.log(final_price);
+    }
+    result.append(fullNameValue); //try the .append to show something in HTML
 });
+
+

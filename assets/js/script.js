5e48fcf8842c832040cdd7ba4635c3ce8f4dbe93
SimoneCerri <iosonosimone@hotmail.it>
2024-02-20 16:29:04 +0100

    change .value position

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,18 @@
 //Create some variables and take it from html
-let fullName = document.querySelector(".full_name");
-let fullNameValue = fullName.value;
+let fullName = document.querySelector(".full_name").value;
+//let fullNameValue = fullName.value;
 
-let kiloMeters = document.querySelector(".kilometers");
-let kiloMetersValue = kiloMeters.value;
+let kiloMeters = document.querySelector(".kilometers").value;
+//let kiloMetersValue = kiloMeters.value;
 
 let submit = document.querySelector(".submit_form");
 let reset = document.querySelector(".reset_form");
-console.log(submit);
 
 //Add the click event to get variables
 //document.addEventListener("click",)
 submit.addEventListener("click" , function() {
-    console.log(fullNameValue);
-    console.log(kiloMetersValue);
+    console.log(fullName);
+    console.log(kiloMeters);
     let result = document.querySelector(".ticket");
-    result.append(fullNameValue);
+    result.append(fullName);
 });

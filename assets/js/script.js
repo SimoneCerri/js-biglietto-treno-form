4c7be8eaee62a14bd684ca3937bfeb936c6819ec
SimoneCerri <iosonosimone@hotmail.it>
2024-02-20 15:22:55 +0100

    restart the exercise with a different method

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+//Create some variables and take it from html
+let fullName = document.getElementsByName("fullname");
+console.log(fullName);
+
+//Add the click event to get variables
+//document.addEventListener("click",)
+//document.getElementsByName
+
+
+//console.log(kilometers);
